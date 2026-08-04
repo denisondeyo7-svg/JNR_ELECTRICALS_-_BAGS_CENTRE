68b3ed0097e1117d422ff3e4700bab0e991a971c
Denis <desnisondeyo7@gmail.com>
2026-08-04 16:53:32 +0300

    save changes

--- backend/config.php
+++ backend/config.php
@@ -2,9 +2,9 @@
 
 
 
-$host = "://infinityfree.com"; // 1. CHANGE THIS to your MySQL Hostname
-$username = "if0_42538586";        // 2. Your exact account username
-$password = "1290Den001";       // 3. CHANGE THIS to your revealed password
+$host = "sql208.infinityfree.com";
+$username = "if0_42538586";       
+$password = "1290Den001";      
 $dbname = "if0_42538586_jnr_db"
 
 $connection = mysqli_connect($host,$username,$password,$dbname);
