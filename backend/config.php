b07d082824b75c9c38b7e91ed6fb009bd96c6bad
Denis <desnisondeyo7@gmail.com>
2026-08-04 14:00:17 +0300

    save changes

--- backend/config.php
+++ backend/config.php
@@ -1,6 +1,13 @@
 <?php
 
-$connection = mysqli_connect("localhost","root","","JNR");
+
+
+$host = "://infinityfree.com"; // 1. CHANGE THIS to your MySQL Hostname
+$username = "if0_42538586";        // 2. Your exact account username
+$password = "1290Den001";       // 3. CHANGE THIS to your revealed password
+$dbname = "if0_42538586_jnr_db"
+
+$connection = mysqli_connect($host,$username,$password,$dbname);
 
 if(!$connection){
     die("Connection failed please try again");
