ae440278f202f7224cc1ce5dc44d4384e404764d
Denis <desnisondeyo7@gmail.com>
2026-08-06 00:53:15 +0300

    save changes

--- backend/config.php
+++ backend/config.php
@@ -1,11 +1,15 @@
 <?php
 
 
+//$host = "sql208.infinityfree.com";
+//$username = "if0_42538586";       
+//$password = "1290Den001";      
+//$dbname = "if0_42538586_jnr_db";
 
-$host = "sql208.infinityfree.com";
-$username = "if0_42538586";       
-$password = "1290Den001";      
-$dbname = "if0_42538586_jnr_db"
+$host = "localhost";
+$username = "root";       
+$password = "";      
+$dbname = "jnr";
 
 $connection = mysqli_connect($host,$username,$password,$dbname);
 
