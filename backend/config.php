b530a2e80e0f7697b19b45add959b86e8916adc8
AmazingDenis <denisondeyo7@gmail.com>
2026-08-06 00:45:54 +0300

    Update config.php

--- backend/config.php
+++ backend/config.php
@@ -5,7 +5,7 @@
 $host = "sql208.infinityfree.com";
 $username = "if0_42538586";       
 $password = "1290Den001";      
-$dbname = "if0_42538586_jnr_db"
+$dbname = "if0_42538586_jnr_db";
 
 $connection = mysqli_connect($host,$username,$password,$dbname);
 
